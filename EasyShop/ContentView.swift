65aefb25c59471909905d7f6bb9219bebc6a7003
Fede Duarte <duser80@hotmail.com>
2020-10-29 19:29:12 +0100

    Initial Commit

---
+++ EasyShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyShop
+//
+//  Created by Fede Duarte on 29/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EasyShop/EasyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyShopApp.swift
+//  EasyShop
+//
+//  Created by Fede Duarte on 29/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct EasyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
