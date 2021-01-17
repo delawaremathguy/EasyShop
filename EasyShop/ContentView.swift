d0e4824443b41953216d5b4e304c4d5b4c9fb1f4
FedeMini <duser80@hotmail.com>
2021-01-17 01:28:13 +0100

    Badge with ZStack circle instead

--- EasyShop/ContentView.swift
+++ EasyShop/ContentView.swift
@@ -6,7 +6,13 @@ struct ContentView: View {
     @State private var selected = 1
     @StateObject private var itemStatusChanged = ItemStatusChanged()
     
+    private var badgePosition: CGFloat = 2
+    private var tabsCount: CGFloat = 3
+    
     var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .bottomLeading) {
+        
         TabView(selection: $selected) {
             
             ShopList().tabItem {
@@ -16,7 +22,7 @@ struct ContentView: View {
             
             SelectedShopView().tabItem {
                 Text("Cart")
-                Image(Item.onShoppingListCount() != 0 ? "shopcartbadge" : "shopcart")
+                Image("shopcart")
             }.tag(1)
             
             SettingsViews().tabItem {
@@ -25,6 +31,14 @@ struct ContentView: View {
             }.tag(2)
             
         }.accentColor(theme.mainColor)
+                ZStack {
+                    Circle().foregroundColor(.red)
+                }
+                .frame(width: 13, height: 13)
+                .offset(x: ( ( 2 * self.badgePosition) - 0.94 ) * ( geometry.size.width / ( 2 * self.tabsCount ) ) + 2, y: -29)
+                .opacity(Item.onShoppingListCount() != 0 ? 1.0 : 0)
+            }
+        }
     }
 }
 
@@ -34,3 +48,31 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+/*
+ struct ContentView: View {
+     @ObservedObject var theme = gThemeSettings
+     @State private var selected = 1
+     @StateObject private var itemStatusChanged = ItemStatusChanged()
+     
+     var body: some View {
+         TabView(selection: $selected) {
+             
+             ShopList().tabItem {
+                 Text("List")
+                 Image("shoplist")
+             }.tag(0)
+             
+             SelectedShopView().tabItem {
+                 Text("Cart")
+                 Image(Item.onShoppingListCount() != 0 ? "shopcartbadge2" : "shopcart")
+             }.tag(1)
+             
+             SettingsViews().tabItem {
+                 Text("Settings")
+                 Image("shopsettings")
+             }.tag(2)
+             
+         }.accentColor(theme.mainColor)
+     }
+ }
+ */
